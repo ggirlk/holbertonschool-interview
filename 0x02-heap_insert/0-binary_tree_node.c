95ac3b98e81b9a1ee269f43103392be8b80f9b49
ggirlk <ggirlk2@gmail.com>
2020-08-08 17:37:29 +0100

    README :(

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -12,7 +12,7 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 binary_tree_t *temp = (binary_tree_t *)malloc(sizeof(binary_tree_t));
-if (binary_tree_t == NULL)
+if (temp == NULL)
 {
 	return(NULL);
 }
