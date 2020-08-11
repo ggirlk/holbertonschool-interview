a82916e3d916ed277346a3d6c7760ec689e999b9
ggirlk <ggirlk2@gmail.com>
2020-08-11 22:42:54 +0100

    min op

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Minimum Operations """
+
+def minOperations(n):
+    count = 0
+    i = 2
+    if n % 2 != 0:
+    	n -= 1
+    while n > 1:
+    	count += 1
+    	count += 1
+    	n = n / 2
+
+    return int(count)
