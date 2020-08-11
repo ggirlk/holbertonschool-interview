ababbcdb91591de817265233b4f108d305c4f42a
ggirlk <ggirlk2@gmail.com>
2020-08-11 22:48:25 +0100

    min op

--- 0x03-minimum_operations/0-main.py
+++ 0x03-minimum_operations/0-main.py
@@ -8,7 +8,7 @@ minOperations = __import__('0-minoperations').minOperations
 n = 4
 print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
 
-n = 12
+n = 1
 print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
 
 n = 9
--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 """ Minimum Operations """
 
+
 def minOperations(n):
     count = 0
-    i = 2
+    if not isinstance(n, int) or n < 0:
+        return 0
+
     if n % 2 != 0:
-    	n -= 1
+        n -= 1
     while n > 1:
-    	count += 1
-    	count += 1
-    	n = n / 2
+        count += 1
+        count += 1
+        n = n / 2
 
     return int(count)
