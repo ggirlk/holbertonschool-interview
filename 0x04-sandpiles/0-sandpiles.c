11a95597f04e991774af1bc7139bd9f6d385233e
khouloud <ggirlk2@gmail.com>
2020-08-16 20:16:46 +0100

    sandpiles

---
+++ 0x04-sandpiles/0-sandpiles.c
@@ -0,0 +1,93 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "sandpiles.h"
+
+/**
+ * print_grid - to print
+ * @grid: grid
+ *
+ * Return: nothing
+ *
+
+static void print_grid(int grid[3][3])
+{
+    int i, j;
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (j)
+                printf(" ");
+            printf("%d", grid[i][j]);
+        }
+        printf("\n");
+    }
+}*/
+
+/**
+ * topple - to topple grid
+ * @grid: grid
+ *
+ * Return: grid
+ */
+
+int topple(int grid[3][3], int k)
+{
+	for (int i = 0; i < 3; ++i)
+	{
+		for (int j = 0; j < 3; ++j)
+		{
+			if (grid[i][j] > 3)
+			{
+				grid[i][j] -= 4;				
+				if ((i - 1) >= 0)
+				{
+					grid[i-1][j] += 1;
+				}
+				if ((j - 1) >= 0)
+				{
+					grid[i][j-1] += 1;
+				}
+				if ((j + 1) < 3)
+				{
+					grid[i][j+1] += 1;
+				}
+				if ((i + 1) < 3)
+				{
+					grid[i+1][j] += 1;
+				}
+				k = 1;				
+			}
+		}		
+	}
+	/* printf("=\n");	
+	// print_grid(grid); */
+	if (k == 1)
+	{
+		grid[3][3] = topple(grid, 0);
+	}	
+	return (grid[3][3]);
+}
+
+/**
+ * sandpiles_sum - to add grids
+ * @grid1: grid
+ * @grid2: grid
+ *
+ * Return: nothing
+ */
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+	int k = 0;
+	for (int i = 0; i < 3; ++i)
+	{
+		for (int j = 0; j < 3; ++j)
+		{
+			grid1[i][j] += grid2[i][j];
+		}
+	}
+	grid1[3][3] = topple(grid1, k);
+}
