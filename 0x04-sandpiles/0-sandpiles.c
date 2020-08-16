4ef27104d3c5eb076dcdecb51417e00c030693db
khouloud <ggirlk2@gmail.com>
2020-08-16 22:06:58 +0100

    spaces

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -9,18 +9,18 @@
 
 static void print_grid(int grid[3][3])
 {
-    int i, j;
+int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (j)
-                printf(" ");
-            printf("%d", grid[i][j]);
-        }
-        printf("\n");
-    }
+for (i = 0; i < 3; i++)
+{
+for (j = 0; j < 3; j++)
+{
+if (j)
+printf(" ");
+printf("%d", grid[i][j]);
+}
+printf("\n");
+}
 }
 /**
  * topple - to topple grid
@@ -32,41 +32,41 @@ static void print_grid(int grid[3][3])
 
 int topple(int grid[3][3], int k)
 {
-	for (int i = 0; i < 3; ++i)
-	{
-		for (int j = 0; j < 3; ++j)
-		{
-			if (grid[i][j] > 3)
-			{
-				grid[i][j] -= 4;
-				if ((i - 1) >= 0)
-				{
-					grid[i - 1][j] += 1;
-				}
-				if ((j - 1) >= 0)
-				{
-					grid[i][j - 1] += 1;
-				}
-				if ((j + 1) < 3)
-				{
-					grid[i][j + 1] += 1;
-				}
-				if ((i + 1) < 3)
-				{
-					grid[i + 1][j] += 1;
-				}
-				k = 1;
-			}
-		}
-	}
+for (int i = 0; i < 3; ++i)
+{
+for (int j = 0; j < 3; ++j)
+{
+if (grid[i][j] > 3)
+{
+grid[i][j] -= 4;
+if ((i - 1) >= 0)
+{
+grid[i - 1][j] += 1;
+}
+if ((j - 1) >= 0)
+{
+grid[i][j - 1] += 1;
+}
+if ((j + 1) < 3)
+{
+grid[i][j + 1] += 1;
+}
+if ((i + 1) < 3)
+{
+grid[i + 1][j] += 1;
+}
+k = 1;
+}
+}
+}
 
-	if (k == 1)
-	{
-		printf("=\n");	
-		print_grid(grid);
-		grid[3][3] = topple(grid, 0);
-	}
-	return (grid[3][3]);
+if (k == 1)
+{
+printf("=\n");
+print_grid(grid);
+grid[3][3] = topple(grid, 0);
+}
+return (grid[3][3]);
 }
 
 /**
@@ -79,14 +79,16 @@ int topple(int grid[3][3], int k)
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-	int k = 0;
+int k = 0;
 
-	for (int i = 0; i < 3; ++i)
-	{
-		for (int j = 0; j < 3; ++j)
-		{
-			grid1[i][j] += grid2[i][j];
-		}
-	}
-	grid1[3][3] = topple(grid1, k);
+for (int i = 0; i < 3; ++i)
+{
+for (int j = 0; j < 3; ++j)
+{
+grid1[i][j] += grid2[i][j];
+}
+}
+printf("=\n");
+print_grid(grid1);
+grid1[3][3] = topple(grid1, k);
 }
