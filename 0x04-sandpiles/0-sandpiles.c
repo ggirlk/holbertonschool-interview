bd2d911f949c6a9c5510d4fd9cb11cf97b246a43
khouloud <ggirlk2@gmail.com>
2020-08-18 15:40:13 +0100

    sandpiles

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -117,9 +117,11 @@ topple(&(*grid1));
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-for (int i = 0; i < 3; ++i)
+int i, j;
+
+for (i = 0; i < 3; ++i)
 {
-for (int j = 0; j < 3; ++j)
+for (j = 0; j < 3; ++j)
 {
 grid1[i][j] += grid2[i][j];
 }
