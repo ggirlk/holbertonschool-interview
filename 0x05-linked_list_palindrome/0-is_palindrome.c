00fccb09a1ce185a63151fe8846a9985066be8e2
khouloud <ggirlk2@gmail.com>
2020-08-24 22:41:55 +0100

    is_palindrome

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - palindrom check
+ * @head: pointer to head of list
+ * Return: 0 or 1
+ */
+
+int is_palindrome(listint_t **head)
+{
+	int size = 0, i, t[100];
+	listint_t **h = head;
+
+	while (*h)
+	{
+		t[size] = (*h)->n;
+		size++;
+		(*h) = (*h)->next;
+	}
+	if (head == NULL)
+	{
+		return (1);
+	}
+	if (size % 2 == 0)
+	{
+		for (i = 0; i < size/2; i++)
+		{
+			if (t[i] != t[size - 1 - i])
+			{
+				return (0);
+			}
+		}
+	}
+	else
+	{
+		for (i = 0; i < size - 1/2; i++)
+		{
+			if (t[i] != t[size - 1 - i])
+			{
+				return (0);
+			}
+		}
+	}
+	return (1);
+}
