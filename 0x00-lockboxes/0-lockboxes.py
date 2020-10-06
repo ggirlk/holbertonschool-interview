cdc0efa8ef67464332c9c0f1f8bc820a3c95c6e6
khouloud <ggirlk2@gmail.com>
2020-10-07 00:55:37 +0100

    lock

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -8,16 +8,19 @@ Created on Mon Jul 27 05:52:28 2020
 
 def canUnlockAll(boxes):
     k = []
-    t = True
-    for i in range(1, len(boxes) - 1):
+    for i in range(0, len(boxes)):
         for j in range(0, len(boxes[i])):
             k.append(boxes[i][j])
 
-    for i in range(1, len(boxes) - 1):
-        if not isinstance(boxes[i], list) and len(boxes[i]) == 0:
-            t = False
-        if i not in k:
-            t = False
-        else:
-            t = True
-    return t
+    i=1
+    try:
+        while boxes[i]:        
+            if not isinstance(boxes[i], list) and len(boxes[i]) == 0:
+                return False
+            if i not in k:
+                return False        
+            i += 1
+    except:
+        return False
+        
+    return True
