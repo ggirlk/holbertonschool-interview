f20b175fa09c2cc9c608013c92e1decda7c3f969
khouloud <ggirlk2@gmail.com>
2020-10-14 04:03:57 +0100

    linked list insertion

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * insert_node - insert node
+ * @head: list
+ * @number: number
+ *
+ * Return: list.
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *h;
+	int t = 0;
+
+	if (!head)
+	{
+		return (NULL);
+	}
+	h = NULL;
+	while (*head)
+	{
+		if ((number < (*head)->n) && t == 0)
+		{
+			add_nodeint_end(&h, number);
+			t = 1;
+		}
+		add_nodeint_end(&h, (*head)->n);
+		*head = (*head)->next;
+	}
+	(*head) = h;
+	return (*head);
+}
