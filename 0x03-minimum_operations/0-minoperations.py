7b5fdb58d9d41fed72e2aa5818eb6d9fe35d99b7
= <ggirlk2@gmail.com>
2021-01-06 23:20:48 +0100

    @_@

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -6,9 +6,22 @@ def minOperations(n):
     count = 0
     if not isinstance(n, int) or n < 0:
         return 0
-
-    k = n**0.5
-    k = round(k*2)
-    if k != n:
-        return 0
-    return int(k)
+    k = 0
+    op = 0
+    while True:
+        if k == 0:
+            count += 1
+            op = (op * 2) + 1
+            k = 2
+        else:
+            count += 1
+            k -= 1
+            op += 1
+        if op >= n:
+            if op == n:
+                return count
+            else:
+                return 0
+        
+        
+        
