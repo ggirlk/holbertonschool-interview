671cc19cf7aa469072a32d713db57164ae23e120
= <ggirlk2@gmail.com>
2021-01-27 16:37:21 +0100

    log parsing

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -4,42 +4,43 @@ import sys
 import signal
 
 
-i = 0
-status = {
-    '200': 0,
-    '301': 0,
-    '400': 0,
-    '401': 0,
-    '403': 0,
-    '404': 0,
-    '405': 0,
-    '500': 0
-}
-fileSize = 0
-
-
-def printstats(fileSize, status):
-    """ doc """
-    print("File size: {}".format(fileSize))
+if __name__ == "__main__":
+    i = 0
+    status = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0
+    }
     fileSize = 0
-    for key in status:
-        if status[key] != 0:
-            print("{}: {}".format(key, status[key]))
-    return 0
 
-for line in sys.stdin:
 
-    def handler(signum, frame):
+    def printstats(fileSize, status):
         """ doc """
-        printstats(fileSize, status)
-        raise KeyboardInterrupt
+        print("File size: {}".format(fileSize))
+        fileSize = 0
+        for key in status:
+            if status[key] != 0:
+                print("{}: {}".format(key, status[key]))
+        return 0
 
-    signal.signal(signal.SIGINT, handler)
+    for line in sys.stdin:
 
-    words = line.split()
-    if words[-2] in status.keys():
-        status[words[-2]] += 1
-    fileSize += int(words[-1])
-    i += 1
-    if i == 10:
-        i = printstats(fileSize, status)
+        def handler(signum, frame):
+            """ doc """
+            printstats(fileSize, status)
+            raise KeyboardInterrupt
+
+        signal.signal(signal.SIGINT, handler)
+
+        words = line.split()
+        if words[-2] in status.keys():
+            status[words[-2]] += 1
+        fileSize += int(words[-1])
+        i += 1
+        if i == 10:
+            i = printstats(fileSize, status)
