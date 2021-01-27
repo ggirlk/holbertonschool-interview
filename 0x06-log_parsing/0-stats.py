3509b25de8a2c2078ba758d279837e2a1f481743
= <ggirlk2@gmail.com>
2021-01-27 17:23:58 +0100

    log parsing

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -37,6 +37,6 @@ if __name__ == "__main__":
                 i = -1
             i += 1
 
-    except (KeyboardInterrupt, SystemExit):
+    except Exception:
         printstats(fileSize, status)
         raise
