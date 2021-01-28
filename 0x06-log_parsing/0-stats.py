2e7d76b39bfcba4cef62b7879fed121613444f97
= <ggirlk2@gmail.com>
2021-01-28 19:19:38 +0100

    Miguel Antonio code

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -14,21 +14,20 @@ if __name__ == "__main__":
         for line in sys.stdin:
             parsed = line.split()
 
-            num += 1
+            if len(parsed) >= 2:
+                total_size += int(parsed[-1])
+                code = int(parsed[-2])
 
-            total_size += int(parsed[-1])
-            code = int(parsed[-2])
+                if code in status_codes.keys():
+                    status_codes[code] += 1
+                else:
+                    status_codes[code] = 1
+                num += 1
+                if (num % 10 == 0):
+                    print("File size: {:d}".format(total_size))
 
-            if code in status_codes.keys():
-                status_codes[code] += 1
-            else:
-                status_codes[code] = 1
-
-            if (num % 10 == 0):
-                print("File size: {:d}".format(total_size))
-
-                for c in sorted(status_codes):
-                    print("{:d}: {:d}".format(c, status_codes[c]))
+                    for c in sorted(status_codes):
+                        print("{:d}: {:d}".format(c, status_codes[c]))
 
         print("File size: {:d}".format(total_size))
 
