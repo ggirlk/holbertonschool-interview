e27e36b54884909cf0d83fbe03184f1413a6b24b
= <ggirlk2@gmail.com>
2021-01-28 19:45:45 +0100

    Miguel Antonio code

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -2,12 +2,12 @@
 """Reads stdin line by line and computes metrics"""
 
 import sys
-import traceback
+
 
 if __name__ == "__main__":
     total_size = 0
-    status_codes = {200:0, 301:0, 400:0, 401:0,
-                   403:0, 404:0, 405:0, 500:0}
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
+                    403: 0, 404: 0, 405: 0, 500: 0}
     num = 0
 
     def printStats(status_codes, total_size):
@@ -22,11 +22,10 @@ if __name__ == "__main__":
             parsed = line.split()
 
             if len(parsed) >= 2:
-                total_size += int(parsed[-1])
                 code = int(parsed[-2])
-
                 if code in status_codes.keys():
                     status_codes[code] += 1
+                total_size += int(parsed[-1])
                 num += 1
                 if (num % 10 == 0):
                     printStats(status_codes, total_size)
@@ -35,4 +34,3 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         printStats(status_codes, total_size)
         raise
-
