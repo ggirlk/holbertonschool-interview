62b94e8b9b86bc867880fa8c3c8609248f2134d2
= <ggirlk2@gmail.com>
2021-02-17 12:51:30 +0100

    utf 8

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -7,10 +7,10 @@ def validUTF8(data):
     i = 0
     for ints in data:
         if i < 129:
-            if (ints.bit_length() > 9):
+            if (ints.bit_length() > 7):
                 return False
         else:
-            if (ints.bit_length() > 11):
+            if (ints.bit_length() > 9):
                 return False
         i += 1
     return True
