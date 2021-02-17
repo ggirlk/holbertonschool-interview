bdd83a6706c3ae1f9890c61416fcab55dc39b61c
= <ggirlk2@gmail.com>
2021-02-17 12:19:01 +0100

    utf 8

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -4,8 +4,10 @@
 
 def validUTF8(data):
     """ doc """
+    if type(data) is not list:
+        return False
     for ints in data:
         if (ints.bit_length() > 8
             or type(ints) is not int):
-            return False
+             return False
     return True
