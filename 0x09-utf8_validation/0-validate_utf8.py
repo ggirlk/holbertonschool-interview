ebbf110d1cf41a8bf24728fd4654a6caa5c2190d
= <ggirlk2@gmail.com>
2021-02-18 13:28:29 +0100

    utf 8

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -5,7 +5,11 @@
 def validUTF8(data):
     """ doc """
     for ints in data:
-        if (ints.bit_length() >= 8):
+        k = (ints.bit_length() << 7)
+        if k > 1024:
             return False
+
+        """if (ints.bit_length() >= 8):
+            return False"""
 
     return True
