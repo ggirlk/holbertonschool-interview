15a1c38a473a882953537496e7822b2b5455be6d
= <ggirlk2@gmail.com>
2021-03-18 19:53:55 +0100

    nqueens

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -8,12 +8,12 @@ if __name__ == '__main__':
         try:
             board_size = int(sys.argv[1])
         except Exception:
-            print("N must be a number\n")
+            print("N must be a number")
             exit(1)
         if board_size < 4:
-            print("N must be at least 4\n")
+            print("N must be at least 4")
             exit(1)
 
     else:
-        print("Usage: nqueens N\n")
+        print("Usage: nqueens N")
         exit(1)
