135a7f0f43a6d48c6bd50af30eb86a7fcdd852de
= <ggirlk2@gmail.com>
2021-04-01 19:40:37 +0100

    sorted array to avl tree

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -35,14 +35,12 @@ avl_t *insert(avl_t *root, int *array, int left, int right)
 	int i;
 
 	if ((left > right) || !tmp)
-        return (NULL);
-
-    i = (right + left) / 2;
-    tmp = binary_tree_node(root, array[i]);
-    tmp->left = insert(tmp, array, left, i - 1);
-    tmp->right = insert(tmp, array, i + 1, right);
-    return (tmp);
-
+		return (NULL);
+	i = (right + left) / 2;
+	tmp = binary_tree_node(root, array[i]);
+	tmp->left = insert(tmp, array, left, i - 1);
+	tmp->right = insert(tmp, array, i + 1, right);
+	return (tmp);
 }
 /**
 * sorted_array_to_avl - create avl tree from sorted array
