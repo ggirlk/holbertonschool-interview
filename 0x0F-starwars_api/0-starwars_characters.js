765d716861e44b2b52f773211d9be9ac99b2eda4
= <ggirlk2@gmail.com>
2021-04-24 22:16:27 +0100

    star wars

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const request = require('request');
+var myArgs = process.argv.slice(2);
+
+let url = 'https://swapi-api.hbtn.io/api/films/' + myArgs[0];
+let options = {json: true};
+
+request(url, options, (error, res, body) => {
+    if (error) {
+        return  console.log(error)
+    };
+    if (!error && res.statusCode == 200) {
+        var results = body.characters;
+
+        for (var i = 0; i < results.length; i++) {
+            request(results[i], options, (error1, res1, body1) => {
+                if (!error1 && res1.statusCode == 200) {
+                  console.log(body1.name)
+                }
+            });
+        }
+        
+    };
+});
