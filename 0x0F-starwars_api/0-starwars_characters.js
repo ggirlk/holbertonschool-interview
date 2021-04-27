40a724f1955047e312241c02bce7650c824d8a2f
= <ggirlk2@gmail.com>
2021-04-27 02:42:50 +0100

    star wars api

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,29 +1,27 @@
 #!/usr/bin/node
 
 const request = require('request');
-if (process.argv.length == 3) {
-    let myArgs = process.argv.slice(2);
-    const url = 'https://swapi-api.hbtn.io/api/films/'+myArgs[0];
-    const options = {json: true};
+if (process.argv.length === 3) {
+  const myArgs = process.argv.slice(2);
+  const url = 'https://swapi-api.hbtn.io/api/films/' + myArgs[0];
+  const options = { json: true };
 
-    request(url, options, async function (error, res, body) {
-        if (error) {
-            console.log(error)
-        } else {
-            for (const char of body.characters) {
-                const ret = () => {
-                    return new Promise((resolve, reject) => {
-                        request(char, options, function (err, resp, bodys) {
-                                if (error)
-                                    console.log(error);
-                                else 
-                                    resolve(bodys.name);
-                        });
-                    });
-                };
-                console.log(await ret());
-            }
-        }
-    });
+  request(url, options, async function (error, res, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      for (const char of body.characters) {
+        const ret = () => {
+          return new Promise((resolve, reject) => {
+            request(char, options, function (err, resp, bodys) {
+              if (error) { console.log(error); } else {
+                resolve(bodys.name);
+              }
+            });
+          });
+        };
+        console.log(await ret());
+      }
+    }
+  });
 }
-
