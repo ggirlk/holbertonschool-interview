584f122e879a08ca5dc1839fc8282637d3394453
= <ggirlk2@gmail.com>
2021-06-09 10:15:42 +0100

    heap sort

---
+++ 0x11-heap_sort/0-heap_sort.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+/**
+ * swap - swaps two integers
+ * @a: int
+ * @b: int
+ * Retuen: nothing
+ */
+void swap(int *x, int *y)
+{
+	*x = *x * *y;
+	*y = *x / *y;
+	*x = *x / *y;
+}
+
+/**
+ * getH - turns an array into a max heap
+ * @array: array of integers
+ * @idx: current given index within array
+ * @size: size of the array
+ * @len: size to use as boundaries
+ * Return: nothing
+ */
+void getH(int *array, int idx, int size, int len)
+{
+	int left = (idx * 2) + 1;
+	int right = (idx * 2) + 2;
+	int max = idx;
+
+	if (left > 0 && left < len && array[left] > array[max])
+		max = left;
+	if (right > 0 && right < len && array[right] > array[max])
+		max = right;
+	if (max != idx)
+	{
+		swap(array + max, array + idx);
+		print_array(array, size);
+		getH(array, max, size, len);
+	}
+}
+/**
+ * heap_sort - sorts an array of integers in ascending order using the Heap sort algorithm
+ * @array: array of integers to sort
+ * @size: size of the array
+ * Return: nothing
+ */
+void heap_sort(int *array, size_t size)
+{
+	int i = (size / 2) - 1;
+	int end = size - 1;
+
+	while (i >= 0)
+	{
+		getH(array, i, size, size);
+		i--;
+	}
+
+	while (end > 0)
+	{
+		swap(array + end, array);
+		print_array(array, size);
+		getH(array, 0, size, end);
+		end--;
+	}
+
+}
---
+++ 0x11-heap_sort/sort.h
@@ -0,0 +1,7 @@
+#ifndef HEAP_SORT
+#define HEAP_SORT
+
+void heap_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+    
+#endif
