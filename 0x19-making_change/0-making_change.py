2afd19bc4fd82f657c56c300924feb6ff1c0a766
= <ggirlk2@gmail.com>
2021-06-24 14:29:41 +0100

    make change 😎

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -20,13 +20,10 @@ def makeChange(coins, total):
             *** If total cannot be met by any number
                 of coins you have, return -1
     """
-    if (type(total) is not int
-       or type(coins) is not list):
+    if (type(total) is not int or type(coins) is not list):
         return -1
     if total <= 0:
         return 0
-    if coins == [1] and total == 2:
-        return 1
     Min = [sys.maxsize for i in range(total+1)]
     Min[0] = 0
     for i in range(1, total+1):
