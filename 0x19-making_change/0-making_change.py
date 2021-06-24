c810d4a8335a16bf88fa30d624617299e3db0732
= <ggirlk2@gmail.com>
2021-06-24 14:20:11 +0100

    make change

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -22,8 +22,7 @@ def makeChange(coins, total):
     """
     if (type(total) is not int
         or type(coins) is not list
-        or coins == []
-        or total > 10**4):
+        or coins == []):
         return -1
     if total <= 0:
         return 0
