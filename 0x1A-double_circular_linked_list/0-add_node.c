520319e05fba7bb129dacd4ae400e99af077c630
= <ggirlk2@gmail.com>
2021-07-01 21:24:40 +0100

    betty styling

--- 0x1A-double_circular_linked_list/0-add_node.c
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -5,82 +5,83 @@
 /**
  * add_node_end - Add a new node to the end of a double
  *                circular linked list
- * @List: the list to modify
+ * @list: the list to modify
  * @str: the string to copy into the new node
- * Returns: Address of the new node, or NULL on failure
+ * Return: Address of the new node, or NULL on failure
  */
 List *add_node_end(List **list, char *str)
 {
-   List *ptr = malloc(sizeof(List));  
-   List *temp;   
-    if(ptr == NULL)  
-   {  
-       return (NULL);  
-   }  
-   else  
-   {  
-       ptr->str=str;
-       if((*list) == NULL)  
-       {  
-           (*list) = ptr;  
-           ptr -> next = (*list);   
-           ptr -> prev = (*list);   
-       }  
-       else  
-       {  
-          temp = (*list);  
-          while(temp->next !=(*list))  
-          {  
-              temp = temp->next;  
-          }  
-          temp->next = ptr;  
-          ptr ->prev=temp;  
-          (*list) -> prev = ptr;  
-          ptr -> next = (*list);  
-        }  
-   }
-   return (*list);
-  
+	List *ptr = malloc(sizeof(List));
+	List *temp;
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		ptr->str = str;
+		if ((*list) == NULL)
+		{
+			(*list) = ptr;
+			ptr->next = (*list);
+			ptr->prev = (*list);
+		}
+		else
+		{
+			temp = (*list);
+			while (temp->next != (*list))
+			{
+				temp = temp->next;
+			}
+			temp->next = ptr;
+			ptr->prev = temp;
+			(*list)->prev = ptr;
+			ptr->next = (*list);
+		}
+	}
+	return (*list);
+
 }
 
 /**
  * add_node_begin - Add a new node to the beginning of a
  *                double circular linked list
- * @List: the list to modify
+ * @list: the list to modify
  * @str: the string to copy into the new node
- * Returns: Address of the new node, or NULL on failure
+ * Return: Address of the new node, or NULL on failure
  */
 List *add_node_begin(List **list, char *str)
 {
-   List *ptr = malloc(sizeof(List));  
-   List *temp;  
-   if(ptr == NULL)  
-   {  
-       return (NULL);  
-   }  
-   else  
-   {  
-        ptr->str=str;  
-        if((*list)==NULL)  
-       {  
-          (*list) = ptr;  
-          ptr -> next = (*list);   
-          ptr -> prev = (*list);   
-       }  
-       else   
-       {  
-            temp = (*list);   
-            while(temp -> next != (*list))  
-            {  
-                temp = temp -> next;   
-            }  
-            temp -> next = ptr;  
-            ptr -> prev = temp;  
-            (*list) -> prev = ptr;  
-            ptr -> next = (*list);  
-            (*list) = ptr;  
-       } 
-   }
-   return (*list); 
+	List *ptr = malloc(sizeof(List));
+	List *temp;
 
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		ptr->str = str;
+		if ((*list) == NULL)
+		{
+			(*list) = ptr;
+			ptr->next = (*list);
+			ptr->prev = (*list);
+		}
+		else
+		{
+			temp = (*list);
+			while (temp->next != (*list))
+			{
+				temp = temp->next;
+			}
+			temp->next = ptr;
+			ptr->prev = temp;
+			(*list)->prev = ptr;
+			ptr->next = (*list);
+			(*list) = ptr;
+		}
+	}
+	return (*list);
 }
