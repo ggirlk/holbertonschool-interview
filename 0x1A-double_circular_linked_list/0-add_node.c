ee306cd5b27ac1ccbef46af2713571fc0a223a8a
= <ggirlk2@gmail.com>
2021-07-01 20:32:00 +0100

    Double Circular Linked List

---
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
+
+/**
+ * add_node_end - Add a new node to the end of a double
+ *                circular linked list
+ * @List: the list to modify
+ * @str: the string to copy into the new node
+ * Returns: Address of the new node, or NULL on failure
+ */
+List *add_node_end(List **list, char *str)
+{
+   List *ptr = malloc(sizeof(List));  
+   List *temp;   
+    if(ptr == NULL)  
+   {  
+       return (NULL);  
+   }  
+   else  
+   {  
+       ptr->str=str;
+       if((*list) == NULL)  
+       {  
+           (*list) = ptr;  
+           ptr -> next = (*list);   
+           ptr -> prev = (*list);   
+       }  
+       else  
+       {  
+          temp = (*list);  
+          while(temp->next !=(*list))  
+          {  
+              temp = temp->next;  
+          }  
+          temp->next = ptr;  
+          ptr ->prev=temp;  
+          (*list) -> prev = ptr;  
+          ptr -> next = (*list);  
+        }  
+   }
+   return (*list);
+  
+}
+
+/**
+ * add_node_begin - Add a new node to the beginning of a
+ *                double circular linked list
+ * @List: the list to modify
+ * @str: the string to copy into the new node
+ * Returns: Address of the new node, or NULL on failure
+ */
+List *add_node_begin(List **list, char *str)
+{
+   List *ptr = malloc(sizeof(List));  
+   List *temp;  
+   if(ptr == NULL)  
+   {  
+       return (NULL);  
+   }  
+   else  
+   {  
+        ptr->str=str;  
+        if((*list)==NULL)  
+       {  
+          (*list) = ptr;  
+          ptr -> next = (*list);   
+          ptr -> prev = (*list);   
+       }  
+       else   
+       {  
+            temp = (*list);   
+            while(temp -> next != (*list))  
+            {  
+                temp = temp -> next;   
+            }  
+            temp -> next = ptr;  
+            ptr -> prev = temp;  
+            (*list) -> prev = ptr;  
+            ptr -> next = (*list);  
+            (*list) = ptr;  
+       } 
+   }
+   return (*list); 
+
+}
---
+++ 0x1A-double_circular_linked_list/README.md
@@ -0,0 +1,15 @@
+# 0x1A. Double Circular Linked List
+<hr>
+<img src="https://static.javatpoint.com/ds/images/circular-doubly-linked-list.png">
+
+## ✍️ Insertion
+<hr>
+
+### 👉 Begining
+<img src="https://static.javatpoint.com/ds/images/insertion-in-circular-doubly-linked-list-at-beginning.png">
+
+### 👉 End
+<img src="https://static.javatpoint.com/ds/images/insertion-in-circular-doubly-linked-list-at-end.png">
+
+
+### 👉 <a href="https://www.javatpoint.com/circular-doubly-linked-list">Full guide</a>
