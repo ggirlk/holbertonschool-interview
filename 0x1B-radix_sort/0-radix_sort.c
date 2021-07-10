7f7bee8a333fa7411b85e1bea6084636a69f8597
= <ggirlk2@gmail.com>
2021-07-10 08:30:59 +0100

    try LSD radix sort

---
+++ 0x1B-radix_sort/0-radix_sort.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * radix_sort - sorts an array of integers in ascending
+ * ***********  order using the Radix sort algorithm
+ * @array: array to be sorted contains only numbers >= 0
+ * @size: array size
+ * Return: nothing
+ */
+void radix_sort(int *array, size_t size)
+{
+    int R = 256, i, r;
+    int N = size-1;
+    int *aux, *count;
+
+    aux =  malloc(sizeof(array));
+    
+    count = malloc(sizeof(int) * (R+1));
+
+    for (i=0; i <= N; i++)
+        count[array[i]+1]++;
+
+    for (r=0; r < R; r++)
+        count[r+1] += count[r];
+
+    for (i=0; i <= N; i++)
+        aux[count[array[i]]++] = array[i];
+
+    print_array(array, size);
+    printf("\n");
+    for (i=0; i <= N; i++)
+        array[i] = aux[i];
+    
+    //free(aux);
+    //free(count);
+
+}
---
+++ 0x1B-radix_sort/sort.h
@@ -0,0 +1,7 @@
+#ifndef SORT
+#define SORT
+
+void radix_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+
+#endif
