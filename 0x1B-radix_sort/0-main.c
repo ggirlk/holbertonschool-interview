c9180d7349c58313de39b06bdb75bb51c1a5421e
= <ggirlk2@gmail.com>
2021-07-11 23:57:12 +0100

    sort

--- 0x1B-radix_sort/0-main.c
+++ 0x1B-radix_sort/0-main.c
@@ -10,7 +10,9 @@
 int main(void)
 {
     int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
-    int a[] = {80, 50, 10, 77, 99, 0, 53, 84};
+    //int a[] = {80, 50, 10, 77, 99, 0, 53, 84};
+    
+    int a[] = {9, 7,15, 8, 0, 5,18, 1, 3, 2,20}; //{9, 7,15, 8, 0, 5,18, 1, 3, 3, 2,20, 4,17, 6,11,13,10,12,14,19, 16};
     size_t n;
     n = sizeof(array) / sizeof(array[0]);
     print_array(array, n);
@@ -19,6 +21,7 @@ int main(void)
     printf("\n");
     print_array(array, n);
     
+    printf("\n");
     n = sizeof(a) / sizeof(a[0]);
     print_array(a, n);
     printf("\n");
