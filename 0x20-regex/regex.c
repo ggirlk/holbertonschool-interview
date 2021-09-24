cf0cc02abfeea52830b082691be89eca5a932823
khouloud <ggirlk2@gmail.com>
2021-09-24 06:41:45 +0100

    regex

--- 0x20-regex/regex.c
+++ 0x20-regex/regex.c
@@ -1,7 +1,30 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "regex.h"
-#include <regex.h>
+
+/**
+ * compare - compares two strings
+ * @first: first string
+ * @second: second string
+ * Return: 1 if the strings can be considered identical, otherwise return 0
+**/
+
+int compare(const char *first, const char *second)
+{
+
+	if (*first == '\0' && *second == '\0')
+		return (1);
+
+	if (*first == '*' && *(first + 1) != '\0' && *second == '\0')
+		return (0);
+
+	if (*first == '.' || *first == '?' ||  *first == *second)
+		return (compare(first + 1, second + 1));
+
+	if (*first == '*')
+		return (compare(first + 1, second) || compare(first, second + 1));
+	return (0);
+}
 
 /**
  * regex_match - checks whether a given pattern matches a given string
@@ -10,12 +33,7 @@
  * Return: EXIT_SUCCESS or EXIT_FAILURE
  */
 
-int regex_match(const char *str, const char *pattern)
+int regex_match(char const *str, char const *pattern)
 {
-    regex_t re;
-    if (regcomp(&re, pattern, REG_EXTENDED|REG_NOSUB) != 0) return 0;
-    int status = regexec(&re, str, 0, NULL, 0);
-    regfree(&re);
-    if (status != 0) return 0;
-    return 1;
+    return (compare(pattern, str));
 }
